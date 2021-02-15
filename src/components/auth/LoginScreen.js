b18d9d15df7e9d90e696b0844ad5031b0c90776e
rigoriosh <rigoriosh@gmail.com>
2021-02-15 09:45:23 -0500

    begin with redux

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const [fields, handledInputChange] = useForm(
+        {
+            email:'nando@gmail.com',
+            password: '123456'
+        });
+    const {email, password} = fields;
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        console.log(email, password);
+        dispatch(login(12345, 'rigo'));
+    }
     return (
         <>
             <h3 className="auth__title">Login</h3>
-            <form>
-                <input type="text" placeholder="Email" name="email" className="auth__input"/>
-                <input type="password" placeholder="password" name="password" className="auth__input"/>
+            <form onSubmit={handleLogin}>
+                <input type="text" placeholder="Email" name="email" className="auth__input" value={email} onChange={handledInputChange}/>
+                <input type="password" placeholder="password" name="password" className="auth__input" value={password} onChange={handledInputChange}/>
                 <button type="submit" className="btn btn-primary btn-block" disabled={false}>Login</button>
                 
                 <div className="auth__social-networks">
